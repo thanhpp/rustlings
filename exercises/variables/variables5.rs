2e22dfa6741602a967f48f50b464a866b6aaa419
thanhpp <thanhphanphu18@gmail.com>
2022-11-23 16:55:08 +0700

    chore: ✅ intro & variables
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -6,6 +6,7 @@
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
+    let mut number: i32;
     number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
 }
