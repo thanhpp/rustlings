457217dce58752d8bd02a05e5f1c9cdaadd14653
thanhpp <thanhphanphu18@gmail.com>
2022-12-05 11:37:01 +0700

    chore: ✅ done quiz 1
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,13 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_count: i64) -> i64 {
+    if apple_count <= 40 {
+        return 2 * apple_count;
+    }
+    1 * apple_count
+}
 
 // Don't modify this function!
 #[test]
