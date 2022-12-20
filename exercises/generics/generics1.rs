39677e4a6d24e407b1ffcd12b57ecee7995b628d
thanhpp <thanhphanphu18@gmail.com>
2022-12-20 16:37:55 +0700

    chore: ✅ generic: 1, 2. traits: 1, 2, 3, 4, 5
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -1,11 +1,9 @@
 // This shopping list program isn't compiling!
 // Use your knowledge of generics to fix it.
 
-// Execute `rustlings hint generics1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// Execute `rustlings hint generics1` or use the `hint` watch subcommand for a hint.W
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
-    shopping_list.push("milk");
+    let mut shopping_list: Vec<String> = Vec::new();
+    shopping_list.push("milk".to_string());
 }
