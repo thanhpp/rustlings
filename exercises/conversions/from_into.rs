fc7f9cb9fed446a7dfa822152f1aa7c602bed460
thanhpp <thanhphanphu18@gmail.com>
2022-12-28 15:30:40 +0700

    chore: ✅ rustlings: conversions done
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let splitted: Vec<&str> = s.split(',').collect();
+        if splitted.len() == 2 {
+            if splitted[0].len() == 0 {
+                return Person::default();
+            }
+        } else {
+            return Person::default();
+        }
+
+        match splitted[1].parse::<usize>() {
+            Ok(x) => {
+                return Person {
+                    name: String::from(splitted[0]),
+                    age: x,
+                }
+            }
+            Err(_) => return Person::default(),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,28 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let splitted: Vec<&str> = s.split(',').collect();
+        if splitted.len() < 2 || splitted.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if splitted[0].len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let name = splitted[0].to_string();
+        let age = match splitted[1].parse::<usize>() {
+            Ok(x) => x,
+            Err(err) => return Err(ParsePersonError::ParseInt(err)),
+        };
+
+        Ok(Person {
+            name: name,
+            age: age,
+        })
     }
 }
 
